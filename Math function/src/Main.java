e3b5f09efd6e047ee01683849816b412c3e10747
Santhosh1043k <159894976+Santhosh1043k@users.noreply.github.com>
2024-02-14 11:58:26 +0530

    Add files via upload

---
+++ Math function/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Math.abs(-6.0));
+        System.out.println(Math.floor(6.9));
+        System.out.println(Math.ceil(6.9));
+        System.out.println(Math.max(4,6));
+        System.out.println(Math.min(4,6));
+        System.out.println(Math.pow(5,3));
+        System.out.println(Math.sqrt(9));
+
+    }
+}
